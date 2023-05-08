5f1862ff0bc57682f89e53e2f3dd619143b14bc0
sumit <sumittoppo1111.com>
2023-05-08 06:19:30 +0530

    cors changed

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const app=express();
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extented:true}));
 
-app.use('/',Routes);
-
-dotenv.config();
-
-// const whitelist=["https://bankclient.onrender.com","http://bank-client-qm9s5kspc-sumittoppo1111.vercel.app","https://bank-client-five.vercel.app"]
+// app.use(cors());
 
 const corsOptions={
     
@@ -25,18 +21,17 @@ const corsOptions={
 
 app.use(cors(corsOptions));
 
-// app.use(function(req, res, next) {
-//     // res.header("Access-Control-Allow-Origin", "*");
-//     const allowedOrigins = ["http://localhost:3000","https://bankclient.onrender.com","http://bank-client-qm9s5kspc-sumittoppo1111.vercel.app","https://bank-client-five.vercel.app"];
-//     const origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//          res.setHeader('Access-Control-Allow-Origin', origin);
-//     }
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     res.header("Access-Control-Allow-credentials", true);
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-//     next();
-//   });
+app.use('/',Routes);
+
+dotenv.config();
+
+
+
+
+
+
+
+
 const PORT =process.env.PORT || 8000;
 const username=process.env.DB_USERNAME;
 const password=process.env.DB_PASSWORD;
